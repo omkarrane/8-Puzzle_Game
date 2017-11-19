aa41ea6c3b7d30a88a8004913521f78ee6754a2e
omkarrane <33806156+omkarrane@users.noreply.github.com>
2017-11-20 00:32:55 +0530

    feature-1 Initial commit

---
+++ 8-Puzzle/Puzzle.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Puzzle.aspx.cs" Inherits="_8_Puzzle.Puzzle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="angular.js"></script>
+    <script src="app.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+</head>
+<body ng-app="8-Puzzle" ng-controller="PuzzleController">
+        <table>
+            <tr>
+                <td>{{numberValue[0]}}</td>
+                <td>{{numberValue[1]}}</td>
+                <td>{{numberValue[2]}}</td>
+            </tr>
+            <tr>
+                <td>{{numberValue[3]}}</td>
+                <td>{{numberValue[4]}}</td>
+                <td>{{numberValue[5]}}</td>
+            </tr>
+            <tr>
+                <td>{{numberValue[6]}}</td>
+                <td>{{numberValue[7]}}</td>
+                <td>{{numberValue[8]}}</td>
+            </tr>
+        </table>
+    <button ng-click="newGame()">Click here for a New Game</button>
+    <button ng-click="up()">Up</button>
+    <button ng-click="down()">Down</button>
+    <button ng-click="right()">Right</button>
+    <button ng-click="left()">Left</button>
+</body>
+</html>
---
+++ 8-Puzzle/app.js
@@ -0,0 +1,82 @@
+﻿var app = angular.module("8-Puzzle", []);
+app.controller('PuzzleController', ['$scope',function ($scope) {
+    $scope.numberValue = [];
+    $scope.newGame = function () {
+        var check = 0;
+        var puzzleData = [];
+        var realData = [];
+        while (true) {
+            var index = Math.floor(Math.random() * 10);
+            if (puzzleData.indexOf(index) == -1) {
+                if (index > 0 && index < 10 ) {
+                    puzzleData[check] = index;
+                    if (index == 9) {
+                        realData[check] = ' ';
+                    }
+                    else{
+                    realData[check] = index;
+                    }
+                    check++;
+                }
+            }
+            if (check == 9) { break;}
+        }
+        $scope.numberValue = realData;
+    };
+    $scope.up = function () {
+        var temp = 0;
+        var spaceIndex = $scope.numberValue.indexOf(' ');
+        if (spaceIndex > 2) {
+            temp = $scope.numberValue[spaceIndex];
+            $scope.numberValue[spaceIndex] = $scope.numberValue[spaceIndex - 3];
+            $scope.numberValue[spaceIndex - 3] = temp;
+        }
+        if (winGame() == true) {
+            console.log("You Won");
+        }
+    };
+    $scope.down = function () {
+        var temp = 0;
+        var spaceIndex = $scope.numberValue.indexOf(' ');
+        if (spaceIndex < 6) {
+            temp = $scope.numberValue[spaceIndex];
+            $scope.numberValue[spaceIndex] = $scope.numberValue[spaceIndex + 3];
+            $scope.numberValue[spaceIndex + 3] = temp;
+        }
+        if (winGame() == true) {
+            console.log("You Won");
+        }
+    };
+    $scope.left = function () {
+        var temp = 0;
+        var spaceIndex = $scope.numberValue.indexOf(' ');
+        if (spaceIndex != 0 && spaceIndex != 3 && spaceIndex != 6) {
+            temp = $scope.numberValue[spaceIndex];
+            $scope.numberValue[spaceIndex] = $scope.numberValue[spaceIndex - 1];
+            $scope.numberValue[spaceIndex - 1] = temp;
+        }
+        if (winGame() == true) {
+            console.log("You Won");
+        }
+    };
+    $scope.right = function () {
+        var temp = 0;
+        var spaceIndex = $scope.numberValue.indexOf(' ');
+        if (spaceIndex != 2 && spaceIndex != 5 && spaceIndex != 8) {
+            temp = $scope.numberValue[spaceIndex];
+            $scope.numberValue[spaceIndex] = $scope.numberValue[spaceIndex + 1];
+            $scope.numberValue[spaceIndex + 1] = temp;
+        }
+        if (winGame() == true) {
+            console.log("You Won");
+        }
+    };
+    var winGame = function () {
+        for (var i = 0; i < 8; i++) {
+            if (($scope.numberValue[i]) != (i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}]);
